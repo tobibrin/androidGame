57ff0f8b6036123625aab1e74034833717c8395b
tobibrin <Tobi_Brinker@web.de>
2018-06-12 09:22:45 +0200

    -added vectorDrawables.useSupportLibrary

--- app/src/main/java/school/androidgame/entities/Player.java
+++ app/src/main/java/school/androidgame/entities/Player.java
@@ -3,9 +3,15 @@ package school.androidgame.entities;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.ColorFilter;
 import android.graphics.Paint;
 import android.graphics.PointF;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.VectorDrawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.graphics.drawable.VectorDrawableCompat;
 import android.view.MotionEvent;
 
 import school.androidgame.core.GameObject;
@@ -86,7 +92,13 @@ public class Player extends GameObject {
         BitmapColor bitmapColorRed = new BitmapColor(redImage, ObjectColorState.COLOR_STATE_RED);
 
         this.bitmapColorRepository.addBitmapColors(new BitmapColor[]{bitmapColorBlue, bitmapColorGreen, bitmapColorRed});
+//        this.drawableTest = ; //TODO
+
+
+
     }
+
+    private VectorDrawableCompat drawableTest;
 
     @Override
     public void update(float dt) {
